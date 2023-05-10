31e0a4408fdfac30df2faee8edbaabe12df02e62
user <p.yarcko@gmail.com>
2023-05-10 23:31:46 +0300

    create get-delta-rate

--- src/get-delta-rate.js
+++ src/get-delta-rate.js
@@ -1,24 +1,33 @@
+/**
+ * 
+ * @param {number} ratePlayer 
+ * @param {number} rateRival 
+ * @returns {number} player rating after the match
+ */
 const ratingCalculation = (ratePlayer, rateRival) => {
-  let result = 0;
-  if (ratePlayer >= rateRival) {
-    if (ratePlayer - rateRival <= 2) {
-      return ratePlayer + 2;
+  if (typeof ratePlayer !== 'number' || typeof rateRival !== 'number' || ratePlayer < 0 || rateRival < 0) {
+    console.log('Enter wrong number');
+  } else {
+    if (ratePlayer >= rateRival) {
+      if (ratePlayer - rateRival <= 2) {
+        return ratePlayer + 2;
+      }
+      if (ratePlayer - rateRival > 2 && ratePlayer - rateRival < 20) {
+        return ratePlayer + 1;
+      }
+      else {
+        return ratePlayer;
+      }
     }
-    if (ratePlayer - rateRival > 2 && ratePlayer - rateRival < 20) {
-      return ratePlayer + 1;
+    if (ratePlayer === 0) {
+      ratePlayer = rateRival;
+      return ratePlayer;
     }
-    else {
+    if (ratePlayer < rateRival) {
+      ratePlayer = ratePlayer + (rateRival - ratePlayer + 5) / 3;
       return ratePlayer;
     }
   }
-  if (rateRival > ratePlayer) {
-    ratePlayer = + (rateRival - ratePlayer + 5) / 3;
-    return ratePlayer;
-  }
-  if (ratePlayer === 0) {
-    ratePlayer === rateRival;
-    return ratePlayer;
-  }
-  
 }
-console.log(ratingCalculation(1, 16));
+console.log(ratingCalculation(25, 10));
+
