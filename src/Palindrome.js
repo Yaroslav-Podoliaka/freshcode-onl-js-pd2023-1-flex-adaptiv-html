9a60e8b3c4b8d761b640bde0b772fead7eb1bc99
user <p.yarcko@gmail.com>
2023-05-17 12:40:35 +0300

    fixed function wordPalindrome

--- src/Palindrome.js
+++ src/Palindrome.js
@@ -1,25 +1,8 @@
 'use strict';
-const str3 = 'aBcba'
-function palindrome(str) {
-  let separator = '';
-  let array_1 = str.toUpperCase().split(separator);
-  let array_2 = str.toUpperCase().split(separator).reverse();
-  for (let i = 0; i < array_1.length; i++){
-    if (array_1[i] !== array_2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-console.log(palindrome(str3));
-
+const str3 = 'aBcbA'
 function wordPalindrome(str) {
   let separator = '';
-  if (str.toUpperCase().split(separator).join(separator) ===
-    str.toUpperCase().split(separator).reverse().join(separator)) {
-    return true;
-  }
-  return false;
+  return str.toUpperCase() === str.toUpperCase().split(separator).reverse().join(separator);
 }
 console.log(wordPalindrome(str3));
 
